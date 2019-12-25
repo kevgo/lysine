cbc089934282a0d53e03c7fb46e4532b6c250912
Kevin Goslar <kevin.goslar@gmail.com>
2019-12-25 13:27:10 -0600

    Unit tests for isLetter

---
+++ test/is-letter-test.mjs
@@ -0,0 +1,13 @@
+import { isLetter } from "../index-5b1e.js"
+import { test, assert } from "./framework.mjs"
+
+test("isLetter", async function() {
+  const letters = ["a", "b", "z", "A", "B", "Z"]
+  for (const letter of letters) {
+    assert(isLetter(letter), true, letter)
+  }
+  const nonLetters = ["1", "9", "0", "!", "%", "("]
+  for (const nonLetter of nonLetters) {
+    assert(!isLetter(nonLetter), true, nonLetter)
+  }
+})
